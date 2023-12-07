0b46510c495eac214070b7a340d80a6e29813954
relentless-coder <contact@ayushbahuguna.com>
2023-12-07 09:32:06 +0530

    disable git

--- lua/plugin_config/nvim-tree.lua
+++ lua/plugin_config/nvim-tree.lua
@@ -1,7 +1,11 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+  git = {
+    enable = false,
+  }
+})
 
 vim.keymap.set('n', '<leader>o', ':NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>e', ':NvimTreeFindFile<CR>')
